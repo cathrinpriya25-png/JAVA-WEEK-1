7eaa65f92edab12075f71d46e9cad1fbfdc01ff4
cathrinpriya25-png <cathrinpriya25@gmail.com>
2026-01-07 12:09:36 +0530

    ADDED TASK 6- PrintPairs

--- PrintPairs.java
+++ PrintPairs.java
@@ -3,7 +3,6 @@ public class PrintPairs {
         int[] arr = {1, 2, 3, 4};
 
         System.out.println("All possible pairs:");
-
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 System.out.println("(" + arr[i] + ", " + arr[j] + ")");
