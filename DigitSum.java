ed6d84366c35d0256adde4c189f654a6ca4572e9
cathrinpriya25-png <cathrinpriya25@gmail.com>
2026-01-07 12:15:45 +0530

    Update DigitSum.java

--- DigitSum.java
+++ DigitSum.java
@@ -1,22 +1,25 @@
-@FunctionalInterface
-interface DigitSum {
-    int calculate(int number, String option);
-}
+import java.util.function.BiFunction;
 public class DigitSumLambda {
     public static void main(String[] args) {
-        DigitSum ds = (num, opt) -> {
+
+        // Lambda Expression:
+        // true  -> sum of even digits
+        // false -> sum of odd digits
+        BiFunction<Integer, Boolean, Integer> digitSum = (num, isEven) -> {
             int sum = 0;
             while (num > 0) {
                 int digit = num % 10;
-                if (opt.equalsIgnoreCase("even") && digit % 2 == 0)
+                if (isEven && digit % 2 == 0) {
                     sum += digit;
-                else if (opt.equalsIgnoreCase("odd") && digit % 2 != 0)
+                } else if (!isEven && digit % 2 != 0) {
                     sum += digit;
-                num /= 10;
+                }
+                num = num / 10;
             }
             return sum;
         };
-        System.out.println("Sum of even digits: " + ds.calculate(12345, "even"));
-        System.out.println("Sum of odd digits: " + ds.calculate(12345, "odd"));
-    }
-}
+        int number = 123456;
+        System.out.println("Sum of even digits: " + digitSum.apply(number, true));
+        System.out.println("Sum of odd digits: " + digitSum.apply(number, false));
+    
+
